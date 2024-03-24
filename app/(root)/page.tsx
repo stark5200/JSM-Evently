3b342102593fce4f7ac45297ddaef10f06c12d43
stark5200 <a.taha5200@gmail.com>
2024-03-24 04:14:27 +0100

    added public folder

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">Evently</h1>
-      <Button variant="destructive" className="px-10">Delete</Button>
+      
     </main>
   )
 }
