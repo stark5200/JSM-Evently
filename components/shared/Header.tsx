dddc31c4ed03627a9ef2f3bd4b69f7f3710c3eaa
stark5200 <a.taha5200@gmail.com>
2024-03-25 05:22:15 +0100

    starting clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,19 @@
+// remember import shortcut ctrl + space
+import Image from "next/image"
+import Link from "next/link"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flexitems-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src="/assets/images/logo.svg" width={128} height={38} alt="Evently logo"/>
+          <div className="flex w-32 justify-end gap-3">
+            
+          </div>
+        </Link>
+      </div>
+    </header>
   )
 }
 
