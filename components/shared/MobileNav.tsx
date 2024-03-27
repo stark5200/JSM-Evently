cdc9d0e6a36386e4b02a9a50ef02625488a14f89
stark5200 <a.taha5200@gmail.com>
2024-03-28 00:29:28 +0100

    sheet trigger

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <nav className='md:hidden'>
+      <Sheet>
+        <SheetTrigger className=''>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </nav>
   )
 }
 
