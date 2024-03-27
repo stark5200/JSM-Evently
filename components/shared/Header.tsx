5f9a6b8ccb88cfd12c440406502ba7bb819931a7
stark5200 <a.taha5200@gmail.com>
2024-03-28 00:20:04 +0100

    Mobilr Nav added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
 import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -11,21 +12,28 @@ const Header = () => {
       <div className="wrapper flexitems-center justify-between">
         <Link href="/" className="w-36">
           <Image src="/assets/images/logo.svg" width={128} height={38} alt="Evently logo"/>
-          <div className="flex w-32 justify-end gap-3">
-            <SignedIn>
-              <UserButton afterSignOutUrl="/"/>
-              <NavItems />
-            </SignedIn>
-
-            <SignedOut>
-              <Button asChild className="rounded-full" size="lg">
-                <Link href="/sign-in">
-                  Login
-                </Link>
-              </Button>
-            </SignedOut>
-          </div>
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/"/>
+            <MobileNav />
+          </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   )
