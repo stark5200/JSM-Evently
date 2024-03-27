17e8f9a6486ac145820f2de25ad85a0983dc3f34
stark5200 <a.taha5200@gmail.com>
2024-03-27 23:58:47 +0100

    Signed in User Button

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,5 @@
 // remember import shortcut ctrl + space
-import { SignedOut } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
@@ -11,6 +11,10 @@ const Header = () => {
         <Link href="/" className="w-36">
           <Image src="/assets/images/logo.svg" width={128} height={38} alt="Evently logo"/>
           <div className="flex w-32 justify-end gap-3">
+            <SignedIn>
+              <UserButton afterSignOutUrl="/"/>
+            </SignedIn>
+
             <SignedOut>
               <Button asChild className="rounded-full" size="lg">
                 <Link href="/sign-in">
