bf155ded918bb35172a9b24318ef3a2e1a085ee4
stark5200 <a.taha5200@gmail.com>
2024-03-30 22:21:53 +0100

    added seperator

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -7,7 +7,9 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import { Separator } from "@/components/ui/separator"
 import Image from 'next/image'
+import NavItems from './NavItems'
 
 
 const MobileNav = () => {
@@ -17,8 +19,11 @@ const MobileNav = () => {
         <SheetTrigger className='align-middle'>
           <Image src="/assets/icons/menu.svg" alt="menu" width={24} height={24} className='cursor-pointer'/>
         </SheetTrigger>
+
         <SheetContent className='flex flex-col gap-6 bg-white md:hidden'>
           <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38}/>
+          <Separator className='border border-gray-50'/>
+          <NavItems />
         </SheetContent>
       </Sheet>
     </nav>
