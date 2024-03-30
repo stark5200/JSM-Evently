16983f36df138067ed40176bb845f47f94dbc77d
stark5200 <a.taha5200@gmail.com>
2024-03-30 22:16:22 +0100

    sheet adjusted

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Footer = () => {
   return (
     <footer className="border-t">
-      <div>
+      <div className="wrapper flex flex-center flex-between flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href="/">
           <Image src="/assets/images/logo.svg" alt="logo" width={128} height={128}/>
         </Link>
--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -7,21 +7,18 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import Image from 'next/image'
 
 
 const MobileNav = () => {
   return (
     <nav className='md:hidden'>
       <Sheet>
-        <SheetTrigger className=''>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetTrigger className='align-middle'>
+          <Image src="/assets/icons/menu.svg" alt="menu" width={24} height={24} className='cursor-pointer'/>
+        </SheetTrigger>
+        <SheetContent className='flex flex-col gap-6 bg-white md:hidden'>
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38}/>
         </SheetContent>
       </Sheet>
     </nav>
