f668d15e30e234e548add058337d94256f5e62b6
stark5200 <a.taha5200@gmail.com>
2024-03-30 22:03:44 +0100

    footer added

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div>
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={128}/>
+        </Link>
+
+        <p>2024 Evently All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
