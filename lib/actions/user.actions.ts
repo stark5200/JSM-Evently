e6410a24909b894bb62078ce4d73f42982f35d08
stark5200 <a.taha5200@gmail.com>
2024-04-02 19:53:43 +0200

    utils file

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,12 @@
 'use server'
 
-export const createUser  = async (user) => {
-  
+import { CreateUserParams } from "@/types"
+import { handleError } from "../utils"
+
+export const createUser  = async (user: CreateUserParams) => {
+  try {
+    
+  } catch (error) {
+    handleError(error)
+  }
 }
