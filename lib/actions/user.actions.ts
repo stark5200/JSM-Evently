ac0ca73fa4b09b72be5481d06e2f8f667ff06df6
stark5200 <a.taha5200@gmail.com>
2024-04-03 23:41:54 +0200

    create clerk user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,10 +2,15 @@
 
 import { CreateUserParams } from "@/types"
 import { handleError } from "../utils"
+import { connectToDatabase } from "../database"
+import User from "../database/models/user.model"
 
 export const createUser  = async (user: CreateUserParams) => {
   try {
-    console.log("prin ln")
+    await connectToDatabase();
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error)
   }
